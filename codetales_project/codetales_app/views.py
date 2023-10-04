4520ff97116659a803ab4ccbd2bcdeb237a20f93
Sreegovind Sudarsan <sreegovindsudarsan2003@gmail.com>
2023-10-04 18:44:45 +0530

    Registration and Login

--- codetales_project/codetales_app/views.py
+++ codetales_project/codetales_app/views.py
@@ -14,10 +14,20 @@ def service(request):
     return render(request, 'service.html')
 def reglog(request):
     if(request.method=="POST"):
-        fullname=request.POST.get("fullname")
-        email=request.POST.get("email")
-        password=request.POST.get("password")
-        Registration(FullName=fullname,Email=email,Password=password).save()
-        return render(request,'reglog.html')
+        if 'sup' in request.POST:
+            fullname=request.POST.get("fullname")
+            email=request.POST.get("email")
+            password=request.POST.get("password")
+            Registration(FullName=fullname,Email=email,Password=password).save()
+            return render(request,'index.html')
+        elif 'loin' in request.POST:
+            email=request.POST.get("email")
+            password=request.POST.get("password")
+            logobj=Registration.objects.filter(Email=email,Password=password)
+            if logobj:
+                return render(request,'index.html')
+            else:
+                error_message = "Invalid credentials. Please try again."
+                return render(request, 'reglog.html', {'error_message': error_message})
     else:
         return render(request,'reglog.html')
